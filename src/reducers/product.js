a2b2d6d67a15ef7162aea93866d4dc94bcb0b51b
aruboss <minhhieu1096@gmail.com>
2018-09-16 11:39:57 +0700

    ap dung redux

---
+++ src/reducers/product.js
@@ -0,0 +1,14 @@
+var initialState = [
+    {
+        id : 1,
+        name : 'dd'
+    }
+];
+
+const product = (state = initialState, action) => {
+    switch (action.type){
+        default: return [...state];
+    }
+};
+
+export default product;
