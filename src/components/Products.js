72244f8c89e8c22bca650fb7d4361bffbc32b7f4
aruboss <minhhieu1096@gmail.com>
2018-09-16 16:14:04 +0700

    lấy danh sách sản phẩm trên store hiển thị ra màn hình

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,15 +1,28 @@
 import React, { Component } from 'react';
 import Product from './Product';
+import { connect } from 'react-redux';
 
 class Products extends Component {
+
+    showProducts(products){
+        var result =null;
+
+       if(products.length>0){
+           result = products.map((product, item)=>{
+               return <Product key={item} product={product}/>
+           });
+       }
+
+        return result;
+    }
+
     render() {
+        var {products} = this.props;
         return (
             <section className="section">
-                <h1 className="section-heading">Danh Sách Sản Phẩm</h1>
+                <h1 className="section-heading">Danh Sách Truyện Của Mặc Hương Đồng Xú</h1>
                 <div className="row">
-                    <Product />
-                    <Product />
-                    <Product />
+                    {this.showProducts(products)}
                 </div>
             </section>
 
@@ -17,4 +30,10 @@ class Products extends Component {
     }
 }
 
-export default Products;
+const mapStateToProps = state => {
+    return {
+        products : state.products
+    }
+}
+
+export default connect(mapStateToProps,null)(Products);
