d463d8cf5b4a8a89552c732351658756ccd04b7f
aruboss <minhhieu1096@gmail.com>
2018-09-17 08:21:47 +0700

    ProductsContainer

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,19 +1,6 @@
 import React, { Component } from 'react';
-import Product from './Product';
 
 class Products extends Component {
-
-    showProducts(products){
-        var result =null;
-
-       if(products.length>0){
-           result = products.map((product, item)=>{
-               return <Product key={item} product={product}/>
-           });
-       }
-
-        return result;
-    }
 
     render() {
         var {products} = this.props;
@@ -21,7 +8,7 @@ class Products extends Component {
             <section className="section">
                 <h1 className="section-heading">Danh Sách Truyện Của Mặc Hương Đồng Xú</h1>
                 <div className="row">
-                    {this.showProducts(products)}
+                    {this.props.children}
                 </div>
             </section>
 
--- src/containers/ProductsContainer.js
+++ src/containers/ProductsContainer.js
@@ -1,13 +1,29 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Products from './../components/Products';
+import Product from './../components/Product';
 
 class ProductsContainer extends Component {
+
+    showProducts(products){
+        var result =null;
+
+       if(products.length>0){
+           result = products.map((product, item)=>{
+               return <Product key={item} product={product}/>
+           });
+       }
+
+        return result;
+    }
+
 
     render() {
         var {products} = this.props;
         return (
-            <Products products={ products }/>
+            <Products>
+                 {this.showProducts(products)}  
+            </Products>
         );
     }
 }
