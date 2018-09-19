97b4c7ccb40402775a8596d8555264d980c99502
aruboss <minhhieu1096@gmail.com>
2018-09-19 23:14:49 +0700

    Hiển Thị Giỏ Hàng 2

--- src/containers/CartContainer.js
+++ src/containers/CartContainer.js
@@ -1,37 +1,57 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import Cart from './../components/Cart';
+import CartItem from './../components/CartItem';
+import * as Message from './../constants/Message';
 
 class CartContainer extends Component {
 
+    showCartItem = (cart) => {
+        var result = Message.MSG_CART_EMPTY;
+        if (cart.length > 0) {
+            result = cart.map((item, index) => {
+                return (
+                    <CartItem
+                        key={index}
+                        item={item}
+                        index={index}
+                    />
+                )
+            })
+        }
+        return result;
+    }
+
     render() {
-        var { cart } =  this.props;
-        console.log(cart);
+        var { cart } = this.props;
         return (
-            <div></div>
+            <Cart>
+                {this.showCartItem(cart)}
+            </Cart>
         );
     }
 }
 
 CartContainer.propTypes = {
-    cart : PropTypes.arrayOf(PropTypes.shape({
-        product : PropTypes.shape({
-            id : PropTypes.number.isRequired,
-            name : PropTypes.string.isRequired,
-            image : PropTypes.string.isRequired,
-            description : PropTypes.string.isRequired,
-            price : PropTypes.number.isRequired,
-            inventory : PropTypes.number.isRequired,
-            rating : PropTypes.number.isRequired
+    cart: PropTypes.arrayOf(PropTypes.shape({
+        product: PropTypes.shape({
+            id: PropTypes.number.isRequired,
+            name: PropTypes.string.isRequired,
+            image: PropTypes.string.isRequired,
+            description: PropTypes.string.isRequired,
+            price: PropTypes.number.isRequired,
+            inventory: PropTypes.number.isRequired,
+            rating: PropTypes.number.isRequired
         }).isRequired,
-        quantity : PropTypes.number.isRequired
-        })).isRequired
+        quantity: PropTypes.number.isRequired
+    })).isRequired
 };
 
 const mapStateToProps = state => {
     return {
-        cart : state.cart
+        cart: state.cart
     }
 }
 
-export default connect(mapStateToProps,null)(CartContainer);
+export default connect(mapStateToProps, null)(CartContainer);
